37419a25fe69fdec66b2be32ab86ac450fa720cf
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-19 16:45:34 +0100

    made the bot actually start

---
+++ bot.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.login(process.env.token);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
+const manager = new ShardingManager('./bot.js', { token: process.env.token });
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
