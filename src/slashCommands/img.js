b08989a2f02c39986d2b6ffdbee17e9e4dddebba
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-19 18:34:16 +0100

    img cmd

---
+++ src/slashCommands/img.js
@@ -0,0 +1,41 @@
+/**
+ * This is the most basic slash command example I can offer you guys, it's not
+ * using the builder but that can easily be changed. If you want your slash
+ * commands to be guild specific, you should consult the discordjs.guide for
+ * how to register them to guilds, here's a link;
+ * https://discordjs.guide/interactions/registering-slash-commands.html#guild-commands
+ * 
+ */
+const {embedcolor, botname} = require('../../config.json');
+const fetch = require('node-fetch');
+const SlashCommand = require("../lib/structures/SlashCommandPiece.js");
+const { MessageEmbed } = require('discord.js');
+ module.exports = class Img extends SlashCommand {
+   constructor(context) {
+     super(context, {
+       name: "img",
+       description: "Img",
+       options: [
+           {
+                name:"query",
+                description:"Query the DB",
+                required:"true",
+                type:"STRING"
+           }
+       ],
+       guildOnly: true
+     });
+   }
+ 
+   async run(interaction) {
+    await interaction.deferReply();
+    const query = interaction.options.getString('query');
+    const { file } = await fetch(`https://loremflickr.com/json/g/320/240/${query}/all`).then(response => response.json());
+    console.log(file)
+    const Embed = new MessageEmbed()
+    .setColor(embedcolor)
+    .setTitle(`${botname} | ${query} Image`)
+    .setImage(file)
+    await interaction.editReply({embeds: [Embed]});
+   }
+ };
