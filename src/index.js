bd6d06a0bec4ee2633518604568e41a142a217f9
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-19 17:15:21 +0100

    wiped out all files

--- src/index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: process.env.token });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
