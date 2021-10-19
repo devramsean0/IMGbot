6683032d2014c1e68a3b5811be7c1875753d0fe3
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-19 16:37:34 +0100

    sharding manager

---
+++ index.js
@@ -0,0 +1,4 @@
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
