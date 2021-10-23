e36a7ea064c57ff82e9426d4c0232a4edc644fa2
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-10-23 17:42:40 +0100

    added advanced image data capabilities

--- src/slashCommands/img.js
+++ src/slashCommands/img.js
@@ -1,7 +1,7 @@
 const {embedcolor, botname} = require('../../config.json');
 const fetch = require('node-fetch');
 const SlashCommand = require("../lib/structures/SlashCommandPiece.js");
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
  module.exports = class Img extends SlashCommand {
    constructor(context) {
      super(context, {
@@ -22,11 +22,52 @@ const { MessageEmbed } = require('discord.js');
    async run(interaction) {
     await interaction.deferReply();
     const query = interaction.options.getString('query');
-    const { file } = await fetch(`https://loremflickr.com/json/g/320/240/${query}/all`).then(response => response.json());
+    const { file, license, owner, width, height, filter, tags, tagMode } = await fetch(`https://loremflickr.com/json/g/320/240/${query}/all`).then(response => response.json());
+    // buttons
+    const Row = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+      .setCustomId('imgadvanced')
+      .setLabel('Advanced')
+      .setStyle('DANGER')
+    )
+    const DisabledRow = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+      .setCustomId('imgadvanced')
+      .setLabel('Advanced')
+      .setStyle('DANGER')
+      .setDisabled('true')
+    )
+    // initial embed
     const Embed = new MessageEmbed()
     .setColor(embedcolor)
     .setTitle(`${botname} | ${query} Image`)
     .setImage(file)
-    await interaction.editReply({embeds: [Embed]});
-   }
- };
+    await interaction.editReply({embeds: [Embed], components: [Row]});
+    // collector
+    const collector = interaction.channel.createMessageComponentCollector({ time: 4000 });
+    collector.on('collect', async i => {
+	    if (i.customId === 'imgadvanced') {
+        const AdvancedEmbed = new MessageEmbed()
+        .setColor(embedcolor)
+        .setTitle(`${botname} | ${query} Image`)
+        .setImage(file)
+        .addFields(
+          {name: 'file', value: `${file}`},
+          {name: 'license', value: `${license}`},
+          {name: 'owner', value: `${owner}`},
+          {name: 'width', value: `${width}`},
+          {name: 'height', value: `${height}`},
+          {name: 'filter', value: `${filter}`},
+          {name: 'tags', value: `${tags}`},
+          {name: 'tagMode', value: `${tagMode}`}
+        )
+		    await i.reply({embeds: [AdvancedEmbed], components: [DisabledRow]})
+	    } else return;
+    });
+collector.on('end', collected => {
+interaction.editReply({embeds: [Embed], components: [DisabledRow]});
+});
+}
+};
